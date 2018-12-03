df2049cb665d8340613a79914262180f198ce772
root <root@centos.localdomain>
2018-12-03 00:50:26 -0500

    creating new model for jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    body = models.CharField(max_length = 200)
+    title = models.CharField(max_length = 50)
+    pub_date = models.Datetime
+    def __str__(self):
+        return f'{self.title}'
